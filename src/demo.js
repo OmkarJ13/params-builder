0be2763707794045118d6f43708572b4d192e818
OmkarJ13 <oajoshi13@gmail.com>
2023-11-05 12:28:44 +0530

    Updated demo

--- src/demo.js
+++ src/demo.js
@@ -1,6 +1,7 @@
 import Parser from "./parser.js";
-import { greaterThanOrEqual, lessThan } from "./utility.js";
+import { and, equals, greaterThanOrEqual, lessThan, notEquals } from "./utility.js";
 
+// Can use with multiple operators
 const params = new Parser()
   .equals('name', 'John')
   .oneOf('city', ['London', 'New York'])
@@ -9,9 +10,11 @@ const params = new Parser()
 console.log(params);
 // > {name: 'eq.John', city: 'in.(London,New York)'}
 
+// Even supports complex operations such as and/or and nested operations
 const params2 = new Parser()
   .and(greaterThanOrEqual('amount', 0), lessThan('amount', 100))
+  .or(equals('payment_mode', 'corporate_card'), equals('payment_mode', 'paid_by_company'))
   .build();
 
 console.log(params2);
-// > {and: '(amount.gte.0,amount.lt.100)'}
+// > {and: '(amount.gte.0,amount.lt.100)', or: '(payment_mode.eq.corporate_card,payment_mode.eq.paid_by_company)'}
