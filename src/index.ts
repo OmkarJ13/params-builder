674ab60d208118eb124ebccd36e542f3e9e80b98
OmkarJ13 <oajoshi13@gmail.com>
2023-11-18 15:25:51 +0530

    Minor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import ParamsBuilder from "./params-builder";
 import { equals, notEquals, lessThan, lessThanOrEqualTo, greaterThan, greaterThanOrEqualTo, isOneOf, isNotOneOf, like, notLike, ilike } from "./utility";
 
-console.log(equals('name', 'John'));
 export { ParamsBuilder, equals, notEquals, lessThan, lessThanOrEqualTo, greaterThan, greaterThanOrEqualTo, isOneOf, isNotOneOf, like, notLike, ilike };
